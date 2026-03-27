3d90b1a61cc0d8ceb8a131fc7cd99aad7d471c51
samuel <samuel.e.cabralrs@gmail.com>
2026-03-27 17:04:20 -0300

    feat: gera habilidades dinamicamente com JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("portfolio carregado!")
+let habilidades = ["HTML", "CSS", "Git", "SQL", "JavaScript"]
+
+let lista = document.querySelector(".habilidades-lista")
+lista.innerHTML = ""
+
+habilidades.forEach(function(habilidade) {
+  let item = document.createElement("li")
+  item.textContent = habilidade
+  item.classList.add("habilidade-item")
+  lista.appendChild(item)
+})
